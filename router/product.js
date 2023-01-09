321bca6abe541d035232b12d75f29baa1c0c006d
reyvido <reyvidod@gmail.com>
2023-01-09 13:38:10 +0700

    remove async handler

--- router/product.js
+++ router/product.js
@@ -1,4 +1,3 @@
-const asyncHandler = require("express-async-handler");
 const express = require("express");
 const router = express.Router();
 const Product = require("../models/Product");
@@ -7,39 +6,30 @@ router.get("/", async (req, res) => {
   res.status(200).json("Bodimaji API");
 });
 
-router.get(
-  "/product/",
-  asyncHandler(async (req, res) => {
-    const product = await Product.find({});
+router.get("/product/", async (req, res) => {
+  const product = await Product.find({});
+  res.json(product);
+});
+
+router.get("/product/latest", async (req, res) => {
+  const product = await Product.find().sort({ createdAt: -1 }).limit(3);
+  if (product) {
     res.json(product);
-  })
-);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
 
-router.get(
-  "/product/latest",
-  asyncHandler(async (req, res) => {
-    const product = await Product.find().sort({ createdAt: -1 }).limit(3);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not Found");
-    }
-  })
-);
-
-router.get(
-  "/product/:id",
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error("Product not Found");
-    }
-  })
-);
+router.get("/product/:id", async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not Found");
+  }
+});
 
 router.post("/product", async (req, res) => {
   const newProduct = new Product(req.body);
